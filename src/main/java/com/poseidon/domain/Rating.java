cf54395b021d53227422f65c23a6f93f6fe08ee7
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-12-24 20:24:44 +0100

    feat: complétion des entités

--- src/main/java/com/poseidon/domain/Rating.java
+++ src/main/java/com/poseidon/domain/Rating.java
@@ -1,12 +1,25 @@
 package com.poseidon.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    String moodysRating;
+
+    String sandPRating;
+
+    String fitchRating;
+
+    int orderNumber;
+
 }
--- src/main/java/com/poseidon/domain/Trade.java
+++ src/main/java/com/poseidon/domain/Trade.java
@@ -1,12 +1,59 @@
 package com.poseidon.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import java.sql.Timestamp;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "trade")
 public class Trade {
-    // TODO: Map columns in data table TRADE with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer tradeId;
+
+    String account;
+
+    String type;
+
+    Double buyQuantity;
+
+    Double sellQuantity;
+
+    Double buyPrice;
+
+    Double sellPrice;
+
+    Timestamp tradeDate;
+    
+    String security;
+    
+    String status;
+    
+    String trader;
+    
+    String benchmark;
+    
+    String book;
+    
+    String creationName;
+    
+    Timestamp creationDate;
+    
+    String revisionName;
+    
+    Timestamp revisionDate;
+    
+    String dealName;
+    
+    String dealType;
+    
+    String sourceListId;
+    
+    String side;
+
 }
